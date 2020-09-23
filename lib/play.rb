ab9fdbfa3825a0e1d4e97743b4a9e7bc4b6e43fe
Beksultan <beksultankaiypov@gmail.com>
2020-09-23 20:24:10 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,10 +37,10 @@ end
 
 # Define your play method below
 def play(board)
-    counter = 0
-  while counter < 9 
-    turn(board) 
-    counter +=1
+  counter = 0
+  until counter == 9
+  turn(board)
+  counter += 1
   end
 end
 
